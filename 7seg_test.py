06e96d688cdf0547beab1587f7c2cb1b270d19a9
Your Name <you@example.com>
2017-10-31 20:58:18 +0000

    updated tests

--- 7seg_test.py
+++ 7seg_test.py
@@ -13,3 +13,16 @@ h="XIO-P7"
 mypins = [a,b,c]
 toy = SevSeg(g,f,a,b,e,d)
 toy.glow_these(mypins)
+
+if __name__ == '__main__':
+	# instantiate with or without pins in given order
+	toy = SevSeg(g,f,a,b,e,d)
+	
+	# create array of pins to glow
+	mypins = [a,b,c]
+
+	# to glow a set of pins
+	toy.glow_these(mypins)
+
+	# to glow a set of pins without complement
+	#toy.glow_these(mypins, comp=True)
